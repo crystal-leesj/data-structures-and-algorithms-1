41ce7558db0d6966d660e122b774072c5ba5270b
Zahra <zahram1087@gmail.com>
2018-11-27 18:06:34 -0800

    file structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,31 +11,19 @@ public class Main {
         favAnimals[2]= "Chicken";
         favAnimals[3]= "Pig";
         favAnimals[4]= "Duck";
-        arrayReverse(newArray);
+        arrayReverse(favAnimals);
 
     }
     public static void arrayReverse(String [] arrayToReverse){
         String []  reversArray= new String[arrayToReverse.length];
         int counter =0;
         for(int i = arrayToReverse.length-1; i>=0; i--){
-
                     reversArray[counter] = arrayToReverse[i];
                     counter ++;
 
-
-    }
+            }
         System.out.println(Arrays.toString(reversArray));
 
-
-//        ArrayList <String> favAnimals = new ArrayList<>();
-//        favAnimals.add("Cat");
-//        favAnimals.add("Dog");
-//        favAnimals.add("Chicken");
-//        favAnimals.add("Pig");
-//        favAnimals.add("Duck");
-//        for(int i = favAnimals.size()-1; i<favAnimals.size(); i--){
-//            System.out.println(favAnimals.get(i));
-//        }
 
     }
 }
